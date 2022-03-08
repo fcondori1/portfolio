904c914eb29c1c4139db1c5e08bc698807604cc6
Frankz Condori <frankcondori97@gmail.com>
2022-03-07 22:40:52 -0500

    add animation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import About from './components/About/About';
 import Nav from './components/Nav/Nav';
+import 'animate.css';
 
 function App() {
 	const { pathname } = useLocation();
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Welcome.module.css';
+import 'animate.css';
 
 function Welcome(props) {
     return (
 			<div className={styles.welcomePage}>
 				<h1>Frankz Portfolio</h1>
 				<Link to='/about'>
-					<h2>Click here to get to know a bit more about me!</h2>
+					<h2 className={`${styles.animate__animated} ${styles.animate__pulse} ${styles.animate__infinite}`}>
+						Click here to get to know a bit more about me!
+					</h2>
 				</Link>
 			</div>
 		);
