c21f4f954b8876c5fd6d2614ece2c6f17d9daaff
Frankz Condori <frankcondori97@gmail.com>
2022-03-10 19:31:50 -0500

    update landing page with new animation

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -4,16 +4,17 @@ import styles from './Welcome.module.css';
 import 'animate.css';
 
 function Welcome(props) {
-    return (
-			<div className={styles.welcomePage}>
-				<h1>Frankz Portfolio</h1>
-				<Link to='/about'>
-					<h2 className={`${styles.animate__animated} ${styles.animate__pulse} ${styles.animate__infinite}`}>
-						Click here to get to know a bit more about me!
-					</h2>
-				</Link>
-			</div>
-		);
+	return (
+		<div className={styles.welcomePage}>
+			<h1 className={styles.introPage}>Frankz Portfolio</h1>
+			<Link to='/about'>
+				<h2
+					className={`${styles.animate__animated} ${styles.animate__pulse} ${styles.animate__infinite}`}>
+					Click here to get to know a bit more about me!
+				</h2>
+			</Link>
+		</div>
+	);
 }
 
 export default Welcome;
