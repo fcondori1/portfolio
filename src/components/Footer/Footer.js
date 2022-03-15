163b35c5e9fb40ff9e34a3e5451b387d8cd03ff3
Frankz Condori <frankcondori97@gmail.com>
2022-03-15 19:15:31 -0400

    add animation to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import styles from './Footer.module.css'
+import './Footer.css'
 
 function Footer(props) {
     return (
-			<div>
-				<ul className={styles.foot}>
-					<li className={styles.footers}>
-						<a
-							href='mailto:frankzcondori@gmail.com'
-							target='_blank'
-							rel='noreferrer'>
-							<div className={styles.icons1}></div>
-						</a>
-					</li>
-					<li className={styles.footers}>
+			<div className='footers'>
+				<ul className='foot'>
+					<li className='footers'>
 						<a
 							href='https://github.com/fcondori1'
 							target='_blank'
 							rel='noreferrer'>
-							<div className={styles.icons2}></div>
+							<div className='icons2 animate__animated animate__fadeInLeft'></div>
 						</a>
 					</li>
-					<li className={styles.footers}>
+					<li className='footers'>
 						<a
 							href='https://www.linkedin.com/in/frankz-condori/'
 							target='_blank'
 							rel='noreferrer'>
-							<div className={styles.icons3}></div>
+							<div className='icons3 animate__animated animate__fadeInUp'></div>
+						</a>
+					</li>
+					<li className='footers'>
+						<a
+							href='mailto:frankzcondori@gmail.com'
+							target='_blank'
+							rel='noreferrer'>
+							<div className='icons1 animate__animated animate__fadeInRight'></div>
 						</a>
 					</li>
 				</ul>
